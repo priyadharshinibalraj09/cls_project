e4792cb10ba62f483a3cfdc12c9271cf42fcfdc6
KGCAS STUDENTS <casstudent@kgisledu.com>
2025-12-16 12:14:07 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App(){
+  return(
+  <div>
+        <h1>Rajish</h1>
+        <h2>Naveen</h2>
+        
+      </div>
+  )
+}
+
+export default App
